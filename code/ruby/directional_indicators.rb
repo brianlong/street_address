2f2d052e610f262eabdd6162d811e8f23e32af42
Brian Long <brian.long@firstmoversadvantage.com>
2014-01-04 14:54:20 -0700

    Added some Ruby code

---
+++ code/ruby/directional_indicators.rb
@@ -0,0 +1,14 @@
+DIRECTIONAL_INDICATORS  = {
+  'EAST' => 'E',
+  'NORTH EAST' => 'NE',
+  'NORTH WEST' => 'NW',
+  'NORTH' => 'N',
+  'NORTHEAST' => 'NE',
+  'NORTHWEST' => 'NW',
+  'SOUTH EAST' => 'SE',
+  'SOUTH WEST' => 'SW',
+  'SOUTH' => 'S',
+  'SOUTHEAST' => 'SE',
+  'SOUTHWEST' => 'SW',
+  'WEST' => 'W'
+}
